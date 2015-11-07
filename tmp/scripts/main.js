2b6ef080b9853b3a7e3e9785dc3c7cf26d61dc41
Nguyen tien Si <nguyentiensi@Nguyens-MacBook-Air.local>
2015-11-08 07:58:43 +1100

    Navigation

--- tmp/scripts/main.js
+++ tmp/scripts/main.js
@@ -9245,11 +9245,54 @@ $(function() {
 
 
 
-  var page = getParameterByName('page');
-  var index = 'pages/' + getParameterByName('index');
+	var page = getParameterByName('page');
+	var index = 'pages/' + getParameterByName('index');
 
-  $('#ipad-wrapper .container').find('iframe').attr('src',index);
+	$('#ipad-wrapper .container').find('iframe').attr('src',index);
 
+
+
+
+	$('#event-zone').mousemove(function(e){
+		$('.xy').text(e.offsetX + ' ' + e.offsetY);
+
+		var x = e.offsetX;
+		var y = e.offsetY;
+	
+		if (y >= 200 && menuToggle) {
+			setTimeout(function(){
+				$('#top-nav').css({'top':-200});
+				$('#info-box .event').text('Event: Menu gone');
+				menuToggle = false;
+			},1000);
+		}
+	});
+
+	var menuToggle = false;
+	$('#event-zone').on('click',function(e){
+
+		var x = e.offsetX;
+		var y = e.offsetY;
+
+		$('.clicktouch').text('Clicked: ' + x + ' ' + y);
+
+		if ((x>=0 && x<=1024) && (y>=0 && y<=200) && !menuToggle) {
+			$('#info-box .event').text('Event: Menu triggered');
+			$('#top-nav').css({'top':0});
+			menuToggle = true;
+		}
+
+
+
+		if (x >=0 && x < 300 && y >= 200) {
+			$('#info-box .event').text('Event: Prev');
+		}
+
+		if (x >= 300 && y >= 200) {
+			$('#info-box .event').text('Event: Next');
+		}		
+
+	});
 
 
 
